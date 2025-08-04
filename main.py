ccc82583c4a6feb37ae1b8c8ff072fdbb368618b
Rohith R Raj <rohithreji2370@gmail.com>
2025-08-04 21:05:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://www.google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://www.youtube.com")
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://www.linkedin.com")
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis.....")
+
+    while True:
+        #Listen for the wake word "Jarvis"
+
+        r = sr.Recognizer()
+        
+        
+    # recognize speech using Google
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Yes sir")
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+                    
+        except Exception as e:
+            print("Error:", e)
